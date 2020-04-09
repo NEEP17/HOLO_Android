34e9e5f5458734955ae0c3c63f154cbe6db3976d
zziyun <jiyun7670@naver.com>
2020-04-09 19:54:24 +0900

    JY 200409_1954 sign up commit

---
+++ app/src/main/java/com/example/holo_android/UI/Activity/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.example.holo_android.UI.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.holo_android.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
