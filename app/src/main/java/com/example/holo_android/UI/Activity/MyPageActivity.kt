85bbf548566f7df8ea22afe07241f122a833958c
paksuua <paksuua@naver.com>
2020-04-15 17:49:01 +0900

    SY 200415_17 add mypage history item.

---
+++ app/src/main/java/com/example/holo_android/UI/Activity/MyPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.holo_android.UI.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.holo_android.R
+
+class MyPageActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_cart)
+    }
+}
